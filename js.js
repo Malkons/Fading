7b0766256e1cec2b82caacf06642cfde95e7443b
Malkons <marc.alkons@gmail.com>
2019-08-13 13:40:28 -0700

    added new if statement

--- js.js
+++ js.js
@@ -12,6 +12,17 @@ $(document).ready(function () {
         explore: ["#tem", "#led", "#sed", "#med"]
     }//end of hideMe object
 
+    var relationshipObj = {
+        gauss: ["tem"],
+        surface: ["tem", "led", "sed", "med"],
+        fiber: ["tem", "led", "sed", "med"],
+        machine: ["led", "sed", "med"],
+        tem: ["gauss", "surface", "fiber"],
+        led: ["surface", "fiber", "machine"],
+        sed: ["surface", "fiber", "machine"],
+        med: ["surface", "fiber"]
+    };//end of relationship Object
+
     var infoObj = {
         details1: {
             title: "Gauss and TEM",
@@ -43,85 +54,28 @@ $(document).ready(function () {
 
     function infoPanel() {
 
-        if (choiceArr[0] === "gauss" && choiceArr[1] === "tem") {
-            alert("gauss and tem");
-        } else if (choiceArr[0] === "tem" && choiceArr[1] === "gauss") {
-            alert("gauss and tem");
-        }
+        var firstChoice = choiceArr[0];
 
-        if (choiceArr[0] === "surface" && choiceArr[1] === "tem") {
-            alert("surface and tem");
-        } else if (choiceArr[0] === "tem" && choiceArr[1] === "surface") {
-            alert("surface and tem");
-        }
+        for (var i = 0; i < relationshipObj[firstChoice].length; i++) {
+             
+            if (relationshipObj[firstChoice][i] === choiceArr[1]) {
+                alert(firstChoice + " " + choiceArr[1]);
+            }//end of conditional
+        }//end of loop
 
-        if (choiceArr[0] === "surface" && choiceArr[1] === "led") {
-            alert("surface and led");
-        } else if (choiceArr[0] === "led" && choiceArr[1] === "surface") {
-            alert("surface and led");
-        }
-
-        if (choiceArr[0] === "surface" && choiceArr[1] === "sed") {
-            alert("surface and sed");
-        } else if (choiceArr[0] === "sed" && choiceArr[1] === "surface") {
-            alert("surface and sed");
-        }
-
-        if (choiceArr[0] === "surface" && choiceArr[1] === "med") {
-            alert("surface and med");
-        } else if (choiceArr[0] === "med" && choiceArr[1] === "surface") {
-            alert("surface and med");
-        }
-
-        if (choiceArr[0] === "fiber" && choiceArr[1] === "tem") {
-            alert("fiber and tem");
-        } else if (choiceArr[0] === "tem" && choiceArr[1] === "fiber") {
-            alert("fiber and tem");
-        }
-
-        if (choiceArr[0] === "fiber" && choiceArr[1] === "led") {
-            alert("fiber and led");
-        } else if (choiceArr[0] === "led" && choiceArr[1] === "fiber") {
-            alert("fiber and led");
-        }
-        
-        if (choiceArr[0] === "fiber" && choiceArr[1] === "sed") {
-            alert("fiber and sed");
-        } else if (choiceArr[0] === "sed" && choiceArr[1] === "fiber") {
-            alert("fiber and sed");
-        }
-
-        if (choiceArr[0] === "fiber" && choiceArr[1] === "med") {
-            alert("fiber and med");
-        } else if (choiceArr[0] === "med" && choiceArr[1] === "fiber") {
-            alert("fiber and med");
-        }
-
-        if (choiceArr[0] === "machine" && choiceArr[1] === "led") {
-            alert("machine and led");
-        } else if (choiceArr[0] === "led" && choiceArr[1] === "machine") {
-            alert("machine and led");
-        }
-
-        if (choiceArr[0] === "machine" && choiceArr[1] === "sed") {
-            alert("machine and sed");
-        } else if (choiceArr[0] === "sed" && choiceArr[1] === "machine") {
-            alert("machine and sed");
-        }
-
-        console.log("User picks =" + " " + choiceArr);
         /*
-                var div = $("<div>" + choiceArr[0] + " " + choiceArr[1] + "</div>");
-                var button = $("<button>");
-                button.html("click me");
-                button.on("click", function () {
-                    reset();
-                });
-                $("#info").append(div);
-                $("#info").append(button);
-                $("#info").show("fast");
                 
-                */
+                        var div = $("<div>" + choiceArr[0] + " " + choiceArr[1] + "</div>");
+                        var button = $("<button>");
+                        button.html("click me");
+                        button.on("click", function () {
+                            reset();
+                        });
+                        $("#info").append(div);
+                        $("#info").append(button);
+                        $("#info").show("fast");
+                        
+                        */
     }//end of infoPanel function
 
     $(".routecard").on("click", function () {
