34529eafd6e59c994ef85160782afd08fb8f4046
Malkons <marc.alkons@gmail.com>
2019-08-20 01:14:57 -0700

    added info object, and logic for infoPanel function

--- js.js
+++ js.js
@@ -13,22 +13,94 @@ $(document).ready(function () {
     }//end of hideMe object
 
     var relationshipObj = {
-        gauss: ["tem"],
-        surface: ["tem", "led", "sed", "med"],
-        fiber: ["tem", "led", "sed", "med"],
-        machine: ["led", "sed", "med"],
-        tem: ["gauss", "surface", "fiber"],
-        led: ["surface", "fiber", "machine"],
-        sed: ["surface", "fiber", "machine"],
-        med: ["surface", "fiber"]
-    };//end of relationship Object
+        gauss: [["tem", "details1"]],
+
+        surface: [
+            ["tem", "details2"],
+            ["led", "details5"],
+            ["sed", "details8"],
+            ["med", "details11"]],
+
+        fiber: [
+            ["tem", "details3"],
+            ["led", "details6"],
+            ["sed", "details9"],
+            ["med", "details12"]],
+
+        machine: [
+            ["led", "details7"],
+            ["sed", "details10"]],
+
+        tem: [
+            ["gauss", "details1"],
+            ["surface", "details2"],
+            ["fiber", "details3"]],
+
+        led: [
+            ["surface", "details5"],
+            ["fiber", "details6"],
+            ["machine", "details7"]],
+
+        sed: [
+            ["surface", "details8"],
+            ["fiber", "details9"],
+            ["machine", "details10"]],
+
+        med: [
+            ["surface", "details11"],
+            ["fiber", "details12"]],
+    };//end relationship object
 
     var infoObj = {
         details1: {
-            title: "Gauss and TEM",
-            info: "Here is some info about Gauss and TEM"
+            title: "Gauss and Tem",
+            info: "Here is some information about Gauss and tem"
+        },
+        details2: {
+            title: "Surface and Tem",
+            info: "Here is some information about Surface and tem"
+        },
+        details3: {
+            title: "Fiber and Tem",
+            info: "Here is some information about Fiber and tem"
+        },
+        details4: {
+            title: "Gauss and Tem",
+            info: "Here is some information about Gauss and tem"
+        },
+        details5: {
+            title: "Surface and led",
+            info: "Here is some information about Surface and led"
+        },
+        details6: {
+            title: "Fiber and led",
+            info: "Here is some information about Fiber and led"
+        },
+        details7: {
+            title: "Machine and led",
+            info: "Here is some information about Machine and led"
+        },
+        details8: {
+            title: "Surface and sed",
+            info: "Here is some information about Surface and sed"
+        },
+        details9: {
+            title: "Fiber and sed",
+            info: "Here is some information about Fiber and sed"
+        },
+        details10: {
+            title: "Machine and sed",
+            info: "Here is some information about Machine and sed"
+        },
+        details11: {
+            title: "Fiber and med",
+            info: "Here is some information about Fiber and med"
+        },
+        details12: {
+            title: "Fiber and med",
+            info: "Here is some information about Fiber and med"
         }
-    }
+    };//end of info object
 
     var choiceArr = [];
 
@@ -53,29 +125,24 @@ $(document).ready(function () {
 
 
     function infoPanel() {
-
         var firstChoice = choiceArr[0];
-
         for (var i = 0; i < relationshipObj[firstChoice].length; i++) {
-             
-            if (relationshipObj[firstChoice][i] === choiceArr[1]) {
-                alert(firstChoice + " " + choiceArr[1]);
-            }//end of conditional
-        }//end of loop
-
-        /*
-                
-                        var div = $("<div>" + choiceArr[0] + " " + choiceArr[1] + "</div>");
+            if (relationshipObj[firstChoice][i][0] === choiceArr[1]) {
+                var test = relationshipObj[firstChoice][i][1];
+                $.each(infoObj, function (key, valueObj) {
+                    if (key === test) {
+                        var infoDiv = $("<div>" + valueObj.title + ":" + " " + valueObj.info + "</div>");
                         var button = $("<button>");
-                        button.html("click me");
+                        button.html("reset");
                         button.on("click", function () {
                             reset();
                         });
-                        $("#info").append(div);
+                        $("#info").append(infoDiv);
                         $("#info").append(button);
-                        $("#info").show("fast");
-                        
-                        */
+                    }//end of inner conditional
+                });//end of inner loop
+            }//end of  outter conditional
+        }//end of loop
     }//end of infoPanel function
 
     $(".routecard").on("click", function () {
--- js1.js
+++ js1.js
@@ -1,21 +1,111 @@
 var relationshipObj = {
-    gauss: ["tem"],
-    surface: ["tem", "led", "sed", "med"],
-    fiber: ["tem", "led", "sed", "med"],
-    machine: ["led", "sed", "med"],
-    tem: ["gauss", "surface", "fiber"],
-    led: ["surface", "fiber", "machine"],
-    sed: ["surface", "fiber", "machine"],
-    med: ["surface", "fiber"]
+    gauss: [["tem", "details1"]],
+
+    surface: [
+        ["tem", "details2"],
+        ["led", "details5"],
+        ["sed", "details8"],
+        ["med", "details11"]],
+
+    fiber: [
+        ["tem", "details3"],
+        ["led", "details6"],
+        ["sed", "details9"],
+        ["med", "details12"]],
+
+    machine: [
+        ["led", "details7"],
+        ["sed", "details10"]],
+
+    tem: [
+        ["gauss", "details1"],
+        ["surface", "details2"],
+        ["fiber", "details3"]],
+
+    led: [
+        ["surface", "details5"],
+        ["fiber", "details6"],
+        ["machine", "details7"]],
+
+    sed: [
+        ["surface", "details8"],
+        ["fiber", "details9"],
+        ["machine", "details10"]],
+
+    med: [
+        ["surface", "details11"],
+        ["fiber", "details12"]],
 };
 
-var choiceArr = ["sed", "surface"];
+var infoObj = {
+    details1: {
+        title: "Gauss and Tem",
+        info: "Here is some information about Gauss and tem"
+    },
+    details2: {
+        title: "Surface and Tem",
+        info: "Here is some information about Surface and tem"
+    },
+    details3: {
+        title: "Fiber and Tem",
+        info: "Here is some information about Fiber and tem"
+    },
+    details4: {
+        title: "Gauss and Tem",
+        info: "Here is some information about Gauss and tem"
+    },
+    details5: {
+        title: "Surface and led",
+        info: "Here is some information about Surface and led"
+    },
+    details6: {
+        title: "Fiber and led",
+        info: "Here is some information about Fiber and led"
+    },
+    details7: {
+        title: "Machine and led",
+        info: "Here is some information about Machine and led"
+    },
+    details8: {
+        title: "Surface and sed",
+        info: "Here is some information about Surface and sed"
+    },
+    details9: {
+        title: "Fiber and sed",
+        info: "Here is some information about Fiber and sed"
+    },
+    details10: {
+        title: "Machine and sed",
+        info: "Here is some information about Machine and sed"
+    },
+    details11: {
+        title: "Fiber and med",
+        info: "Here is some information about Fiber and med"
+    },
+    details12: {
+        title: "Fiber and med",
+        info: "Here is some information about Fiber and med"
+    }
+};
+
+
+
+var choiceArr = ["surface", "sed"];
 
 var firstChoice = choiceArr[0];
 
 for (var i = 0; i < relationshipObj[firstChoice].length; i++) {
-     
-    if (relationshipObj[firstChoice][i] === choiceArr[1]) {
-        alert(firstChoice + " " + choiceArr[1]);
-    }//end of conditional
+    if (relationshipObj[firstChoice][i][0] === choiceArr[1]) {
+        var test = relationshipObj[firstChoice][i][1];
+        $.each(infoObj, function (key, valueObj) {
+            if (key === test) {
+                console.log(valueObj.title);
+                console.log(valueObj.info);
+            }//end of inner conditional
+        });//end of inner loop
+    }//end of  outter conditional
 }//end of loop
+
+
+
+
